2621f1e898e1ccdd0fab081cd8989c03e2aadabb
maskpa100 <root@194-67-111-201.cloudvps.regruhosting.ru>
2022-10-08 15:33:49 +0300

    commit-4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="app-wrapper">
         <h1>сайт создан на react 33</h1>
+        			<div className="h">
+			  <img src="https://anticwar.ru/wp-content/uploads/wp_article/2021-11-23/2021-11-23_677341675205681.jpeg" />
+			  post 2
+			</div>
     </div>
   );
 }
